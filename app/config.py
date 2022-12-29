ee376b4fac68d2bafdea5076550ac5715eff2966
Alex Dvornik <aadvornik@gmail.com>
2022-12-30 01:06:56 +0300

    Created config.py

---
+++ app/config.py
@@ -0,0 +1,21 @@
+import os
+from pydantic import BaseSettings
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    PROJECT_HOST: str
+    PROJECT_PORT: int
+
+
+class Config:
+    env_file = ENV_PATH
+    env_file_encoding = "utf-8"
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
+from app.config import settings, Settings
+
 
 app = FastAPI()
 
@@ -6,3 +9,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main.py",
+        port=int(settings.PROJECT_PORT),
+        host=settings.PROJECT_HOST,
+        reload=True
+    )
