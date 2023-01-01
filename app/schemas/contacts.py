463f25c98e4b2bd8e51411dbb774b5478bd3cdc5
Alex Dvornik <aadvornik@gmail.com>
2023-01-01 21:38:19 +0300

    Updated schemas

--- app/schemas/contacts.py
+++ app/schemas/contacts.py
@@ -3,17 +3,24 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-class BaseContacts(BaseModel):
+class BaseContact(BaseModel):
     first_name: str
     last_name: str
     email: str
     phone_number: str
 
 
-class Contacts(BaseContacts):
+class CreateContact(BaseContact):
+    pass
+
+
+class UpdateContact(BaseContact):
+    updated_at: datetime
+
+
+class Contact(BaseContact):
     id: int
     created_at: datetime
-    updated_at: datetime
 
     class Config:
         orm_mode = True
